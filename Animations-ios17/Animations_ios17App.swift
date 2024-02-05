a58bed1aac8cd4fc9e846117df656f663787fe39
Alin RADU <alin.radu.cristian@gmail.com>
2024-02-05 20:18:48 +0200

    Initial Commit

---
+++ Animations-ios17/Animations_ios17App.swift
@@ -0,0 +1,17 @@
+//
+//  Animations_ios17App.swift
+//  Animations-ios17
+//
+//  Created by Alin RADU on 05.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct Animations_ios17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
